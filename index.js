528611b6001e9076adc4ecd363ca733c8ce226d6
JBWERE-coder <lutherwere@gmail.com>
2023-06-13 23:14:39 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(num1 = 2000, num2 = 100) {
+    return num1/num2;
+}
+divide();
+
+
+const square = (num1) => {
+    return num1**2;
+}
+square(2);
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+add(3,4);
